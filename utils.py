89bbdc025e67da4dde63269f3d016e0b2b126a26
Simone Giovannini <giobba9@gmail.com>
2020-07-26 19:05:39 +0200

    Started clean up

--- utils.py
+++ utils.py
@@ -13,8 +13,10 @@ class CatEncoder(TransformerMixin):
         return self.encoder.transform(X, y)
     
     def set_params(self, **params):
-        print(params)
         return self.encoder.set_params(**params)
+    
+    def get_params(self, **params):
+        return self.encoder.get_params(**params)
 
 # Utility function for evaluation
 def eval_mod(model, X_test, y_test):
@@ -28,29 +30,25 @@ def eval_mod(model, X_test, y_test):
     print('R^2:', r2_score(y_test, y_pred))
 
 # Evaluate encoders
-def eval_encoders(model, X_train, y_train):
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.compose import ColumnTransformer
-    
-    encoders = ['CatBoostEncoder', 'GLMMEncoder', 'JamesSteinEncoder', 
-            'LeaveOneOutEncoder', 'MEstimateEncoder', 'TargetEncoder', 
-            'BinaryEncoder', 'HashingEncoder', 'OrdinalEncoder']
-    
-    num_columns = ['Review Date', 'Cocoa Percent']
-    
-    num_enc = ColumnTransformer(
-    transformers=[
-        ('num', StandardScaler(), num_columns)],
-    remainder='passthrough')
-    
-    scores = []
-    for enc in encoders:
-        pipeline = Pipeline([('cat_enc', CatEncoder(enc)), 
-                             ('scaler', num_enc), 
-                             ('mod', model)])
-        pipeline.fit(X_train, y_train)
-        y_pred = pipeline.predict(X_test)
-        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-        scores.append(rmse)
-    
-    return pd.Series(scores, index=encoders).sort_values()
+# =============================================================================
+# def eval_encoders(model, X_train, y_train, X_test, y_test):
+#     import pandas as pd
+#     import numpy as np
+#     from sklearn.metrics import mean_squared_error
+#     
+#     encoders = ['CatBoostEncoder', 'GLMMEncoder', 'JamesSteinEncoder', 
+#             'LeaveOneOutEncoder', 'MEstimateEncoder', 'TargetEncoder', 
+#             'BinaryEncoder', 'HashingEncoder', 'OrdinalEncoder', 
+#             'OneHotEncoder']
+#     
+#     scores = []
+#     for enc in encoders:
+#         pipeline = Pipeline([('cat_enc', CatEncoder(enc)), 
+#                              ('mod', model)])
+#         pipeline.fit(X_train, y_train)
+#         y_pred = pipeline.predict(X_test)
+#         rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+#         scores.append(rmse)
+#     
+#     return pd.Series(scores, index=encoders).sort_values()
+# =============================================================================
